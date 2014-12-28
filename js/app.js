2eb0d69b7d32eda460c8e660ab24b055a5c6bb7e
David Vanslambrouck <vanslambrouckd@users.noreply.github.com>
2014-12-28 11:59:28 +0100

    -

--- js/app.js
+++ js/app.js
@@ -1,40 +1,58 @@
 var app = {
-	views: {},
-	models: {},
-	loadTemplates: function(views, callback) {
+    views: {},
+    models: {},
+    loadTemplates: function(views, callback) {
 
-		var deferreds = [];
+        var deferreds = [];
 
-		$.each(views, function(index, view) {
-			if (!app[view]) {
-				deferreds.push($.get('js/templates/' + view + '.html', function(data) {
-					app[view] = _.template(data);
-				}, 'html'));
-			} else {
-				alert(view + " not found");
-			}
-		});
+        $.each(views, function(index, view) {
+            if (!app[view]) {
+                deferreds.push($.get('js/templates/' + view + '.html', function(data) {
+                    app[view] = _.template(data);
+                }, 'html'));
+            } else {
+                alert(view + " not found");
+            }
+        });
 
-		$.when.apply(null, deferreds).done(callback);
-	}
+        $.when.apply(null, deferreds).done(callback);
+    }
 };
 
 app.Router = Backbone.Router.extend({
-	routes: {
-		'': 'home'
-	},
-	home: function() {
-		app.headerView = new app.HeaderView({
-			template: app.Header
-		});
-		$('#content').html(app.headerView.render().el);
-	}
+    routes: {
+        '': 'home',
+        'contact': 'contact'
+    },
+    initialize: function() {
+        app.pageView = new app.PageView({
+            template: app.Page
+        });
+
+        console.log('init');
+        $('body').html(app.pageView.render().el);
+        this.$content = $('#content');
+    },
+    home: function() {
+        app.homeView = new app.HomeView({
+            template: app.Home
+        });
+        this.$content.html(app.homeView.render().el);
+        app.pageView.selectMenuItem('menuitem-home');
+    },
+    contact: function() {
+        app.contactView = new app.ContactView({
+            template: app.Contact
+        });
+        this.$content.html(app.contactView.render().el);
+        app.pageView.selectMenuItem('menuitem-contact');
+    }
 });
 
 $(document).on("ready", function() {
-	app.loadTemplates(['Header', 'Contact'],
-		function() {
-			app.router = new app.Router();
-			Backbone.history.start();
-		});
+    app.loadTemplates(['Page', 'Home', 'Contact'], function() {
+        console.log('templates loaded');
+        app.router = new app.Router();
+        Backbone.history.start();
+    });
 });
